394db3111cf5f9f6d245357191f30890d95f4fb4
Everton Lucas <the.evertonlucas@gmail.com>
2026-06-22 12:06:28 -0300

    simplificacao da funcao

--- fibonacci/iterativo/fibonacci_iterativo.c
+++ fibonacci/iterativo/fibonacci_iterativo.c
@@ -1,8 +1,6 @@
 #include "fibonacci_iterativo.h"
 unsigned long long int fibonacci_iterativo(int qtd){
-	int termo1, termo2;
-	long long int soma = 0;
-	termo1 = 0, termo2 = 1; // 2 primeiros termos da sequência
+    unsigned long long int termo1 = 0, termo2 = 1, soma;
 	for (int i=1; i<=qtd; i++)
 		soma = termo1 + termo2, termo1 = termo2, termo2 = soma;
 	return soma;
