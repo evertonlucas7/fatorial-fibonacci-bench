02fe23b6f36b791228eb75ad3589ceb0df89f258
Everton Lucas <the.evertonlucas@gmail.com>
2026-06-22 19:30:41 -0300

    fix: fix do fib iter pra qnd qtd = 1

--- fibonacci/iterativo/fibonacci_iterativo.c
+++ fibonacci/iterativo/fibonacci_iterativo.c
@@ -1,6 +1,8 @@
 #include "fibonacci_iterativo.h"
 unsigned long long int fibonacci_iterativo(int qtd){
     unsigned long long int termo1 = 0, termo2 = 1, soma;
+    if (qtd == 0) return 0;
+    if (qtd == 1) return 1;
     int i;
 	for (i=2; i<=qtd; i++)
 		soma = termo1 + termo2, termo1 = termo2, termo2 = soma;
