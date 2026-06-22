45ea57747d7a86ed646a71233ef04f4ea495be1d
Everton Lucas <the.evertonlucas@gmail.com>
2026-06-22 18:40:01 -0300

    refactor: utilizacao de sintaxe c90 pq o fernando é viadinho e n sabe baixar o gcc

--- fibonacci/iterativo/fibonacci_iterativo.c
+++ fibonacci/iterativo/fibonacci_iterativo.c
@@ -1,7 +1,8 @@
 #include "fibonacci_iterativo.h"
 unsigned long long int fibonacci_iterativo(int qtd){
     unsigned long long int termo1 = 0, termo2 = 1, soma;
-	for (int i=2; i<=qtd; i++)
+    int i;
+	for (i=2; i<=qtd; i++)
 		soma = termo1 + termo2, termo1 = termo2, termo2 = soma;
 	return soma;
 }
