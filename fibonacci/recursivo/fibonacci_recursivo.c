468af7d69b2d6e97f5284d3f2b9330b4c874c349
Everton Lucas <the.evertonlucas@gmail.com>
2026-06-22 12:06:45 -0300

    refactor: simplificacao da funcao

--- fibonacci/recursivo/fibonacci_recursivo.c
+++ fibonacci/recursivo/fibonacci_recursivo.c
@@ -1,12 +1,10 @@
 #include "fibonacci_recursivo.h"
 unsigned long long int fibonacci_recursivo (int posicao)
 {
-	if (posicao == 0){
-		return 0;
-	} // Lembrando que a posição é contada a partir de 0, não de 1
-	if (posicao == 1){ 
+	if (posicao == 0)
+		return 0; // Lembrando que a posição é contada a partir de 0, não de 1
+	if (posicao == 1)
 		return 1;
-	}
 	return fibonacci_recursivo(posicao-1) + fibonacci_recursivo(posicao-2);
 	/* Não vai precisar do laço for, pois usando o 5 como exemplo, como ele não sabe o valor de 5,
 	 * ele vai "guardar" e vai tentar achar o valor de 4, e depois de 3 etc até achar realmente o resultado
